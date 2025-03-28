0544ff38975309d9033661414aa3a16b7211d770
anncli <55011372+anncli@users.noreply.github.com>
2025-03-28 18:57:07 +0100

    Find Alternate Location Names
    Parse the return json to find other location descriptors to use for locations with no compound code

--- google_maps_api.py
+++ google_maps_api.py
@@ -16,15 +16,24 @@ def get_city_name(lat, lng):
             compound_code = reverse_geocode_data["plus_code"]["compound_code"]
             city = compound_code[compound_code.find(" ")+1:]
             return city
-        else:
-            print(f"Compound code not found for {lat}, {lng}")
-            return ""
+        elif "results" in reverse_geocode_data and len(reverse_geocode_data["results"]) > 0:
+            for result in reverse_geocode_data["results"]:
+                location_name = ""
+                if "formatted_address" in result and '+' not in result["formatted_address"]:
+                    location_name = result["formatted_address"]
+                    return location_name
+        
+        # if no location name found that doesn't contain a geocode
+        print(f"Location name not found for {lat}, {lng}")
+        with open("reverse_geocode.json", "a") as file:
+            json.dump(reverse_geocode_data, file)
+        return ""
     else:
         print(f"Failed to fetch reverse geocode data for {lat}, {lng}:", response.status_code)
         return ""
 
 def process_round(round):
-    """Processes a single round entry by adding the city name."""
+    # processes a single round entry by adding the city name
     city = get_city_name(round["lat"], round["lng"])
     round["city"] = city
     return round
@@ -32,25 +41,28 @@ def process_round(round):
 
 def add_city_to_data(game_rounds_file, max_threads=20):
     with open(game_rounds_file, "r") as file:
-        data = json.load(file)
+        rounds_data = json.load(file)
     
     updated_data = []
 
-    # Multithread API requests
+    # multithread API requests
     with ThreadPoolExecutor(max_threads) as executor:
-        future_to_round = {executor.submit(process_round, round): round for round in data}
+        future_to_round = {}
+        for round in rounds_data:
+            future = executor.submit(process_round, round)
+            future_to_round[future] = round
         
         for future in as_completed(future_to_round):
             updated_data.append(future.result())
 
-    # Save updated JSON
+    # save updated JSON
     with open(game_rounds_file, "w") as file:
         json.dump(updated_data, file)
 
 
 if __name__ == "__main__":
     load_dotenv()
-    
+
     game_rounds_file = "game_rounds.json"
     add_city_to_data(game_rounds_file)
     print("City names added to game rounds data.")
